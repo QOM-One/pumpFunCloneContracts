b27c5f00e9decdca5552baa08bb0031b010be3eb
encrypt3dW4lrus <bulion96@hotmail.com>
2024-10-18 00:01:52 +0000

    Added referral parameter in the tests, everything passing

--- test/TokenFactory.js
+++ test/TokenFactory.js
@@ -4,28 +4,31 @@ const {time} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 describe("Token Factory", function () {
     it("Should create the meme token successfully", async function () {
+        const zeroAddress = "0x0000000000000000000000000000000000000000";
         const tokenCt = await hre.ethers.deployContract("TokenFactory");
-        const tx = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", {
-            value: hre.ethers.parseEther("0.0001")
+        const tx = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", zeroAddress, {
+            value: hre.ethers.parseEther("0.0002")
         });
         const memecoins = await tokenCt.getAllMemeTokens();
         console.log("Memecoins ", memecoins)
     });
 
     it("Should revert if incorrect value of memeToken Creation fee is passed", async function () {
+        const zeroAddress = "0x0000000000000000000000000000000000000000";
         const tokenCt = await hre.ethers.deployContract("TokenFactory");
-        await expect(tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", {
-            value: hre.ethers.parseEther("0.00001")
+        await expect(tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", zeroAddress, {
+            value: hre.ethers.parseEther("0.00002")
         })).to.be.revertedWith("fee not paid for memetoken creation");
     });
 
     it("Should allow a user to purchase the meme token", async function() {
+        const zeroAddress = "0x0000000000000000000000000000000000000000";
         const tokenCt = await hre.ethers.deployContract("TokenFactory");
-        const tx1 = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", {
-            value: hre.ethers.parseEther("0.0001")
+        const tx1 = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", zeroAddress, {
+            value: hre.ethers.parseEther("0.0002")
         });
         const memeTokenAddress = await tokenCt.memeTokenAddresses(0)
-        const tx2 = await tokenCt.buyMemeToken(memeTokenAddress, 800000, {
+        const tx2 = await tokenCt.buyMemeToken(memeTokenAddress, 800000, zeroAddress, {
             value: hre.ethers.parseEther("40")
         });
         const memecoins = await tokenCt.getAllMemeTokens();
@@ -33,13 +36,14 @@ describe("Token Factory", function () {
     })
 
     it("Should allow a user to sell the meme token", async function() {
+        const zeroAddress = "0x0000000000000000000000000000000000000000";
         const tokenCt = await hre.ethers.deployContract("TokenFactory");
-        const tx1 = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", {
-            value: hre.ethers.parseEther("0.0001")
+        const tx1 = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", zeroAddress, {
+            value: hre.ethers.parseEther("0.0002")
         });
         const adminAddress = await tokenCt.admin();
         const memeTokenAddress = await tokenCt.memeTokenAddresses(0)
-        const tx2 = await tokenCt.buyMemeToken(memeTokenAddress, 500000, {
+        const tx2 = await tokenCt.buyMemeToken(memeTokenAddress, 500000, zeroAddress, {
             value: hre.ethers.parseEther("15")
         });
         console.log("admin balance before sell", hre.ethers.formatEther(await hre.ethers.provider.getBalance(adminAddress)));
@@ -51,14 +55,15 @@ describe("Token Factory", function () {
         const memecoins = await tokenCt.getAllMemeTokens();
         console.log("Memecoins ", memecoins)
     })
-    it.only("Should allow a user to buy, sell and buy all the meme tokens", async function() {
+    it("Should allow a user to buy, sell and buy all the meme tokens", async function() {
+        const zeroAddress = "0x0000000000000000000000000000000000000000";
         const tokenCt = await hre.ethers.deployContract("TokenFactory");
-        const tx1 = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", {
-            value: hre.ethers.parseEther("0.0001")
+        const tx1 = await tokenCt.createMemeToken("Test", "TEST", "img://img.png", "hello there", zeroAddress, {
+            value: hre.ethers.parseEther("0.0002")
         });
         const adminAddress = await tokenCt.admin();
         const memeTokenAddress = await tokenCt.memeTokenAddresses(0)
-        const tx2 = await tokenCt.buyMemeToken(memeTokenAddress, 500000, {
+        const tx2 = await tokenCt.buyMemeToken(memeTokenAddress, 500000, zeroAddress, {
             value: hre.ethers.parseEther("15")
         });
         console.log("admin balance before sell", hre.ethers.formatEther(await hre.ethers.provider.getBalance(adminAddress)));
@@ -67,7 +72,7 @@ describe("Token Factory", function () {
         await tokenCt.sellMemeToken(memeTokenAddress, 500000)
         console.log("tokens after sell",  hre.ethers.formatEther(await memeToken.balanceOf(adminAddress)));
         console.log("admin balance after sell", hre.ethers.formatEther(await hre.ethers.provider.getBalance(adminAddress)));
-        const tx3 = await tokenCt.buyMemeToken(memeTokenAddress, 800000, {
+        const tx3 = await tokenCt.buyMemeToken(memeTokenAddress, 800000, zeroAddress, {
             value: hre.ethers.parseEther("24")
         });
         const memecoins = await tokenCt.getAllMemeTokens();
